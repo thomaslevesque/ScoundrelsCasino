d57cb3e4f6834832ed55ae85f3a4ec67097e1f7a
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2015-08-07 00:00:13 +0200

    Simplify number generator

--- Roulette/RouletteGenerator.cs
+++ Roulette/RouletteGenerator.cs
@@ -14,9 +14,8 @@ namespace Roulette
         private const int LongestRunAllowed = 2;
 
         private readonly Random _random;
-        private readonly List<int> _lastIndexes = new List<int>();
-        private readonly IDictionary<int, int> _lastOccurrences =
-            Enumerable.Range(0, _numbers.Length).ToDictionary(i => i, i => -1);
+        private readonly List<string> _lastNumbers = new List<string>();
+        private readonly IDictionary<string, int> _lastOccurrences = _numbers.ToDictionary(i => i, i => -1);
 
         private int _turn;
 
@@ -28,32 +27,30 @@ namespace Roulette
 
         public string Next()
         {
-            // Score depends on how long ago the index has last been generated
+            // Score depends on how long ago the number has last been generated
             // more recent = lower score, less recent = higher score
             // Scores start at 1 (0 - (-1))
-            var indexScores = Enumerable.Range(0, _numbers.Length)
-                                .ToDictionary(i => i, i => _turn - _lastOccurrences[i]);
+            var numberScores = _numbers.ToDictionary(i => i, i => _turn - _lastOccurrences[i]);
 
-            // Repeat each index {score} times
-            var indexes =
-                Enumerable.Range(0, _numbers.Length)
-                          .SelectMany(i => Enumerable.Repeat(i, indexScores[i]))
-                          .ToArray();
+            // Repeat each number {score} times
+            var weightedNumbers = _numbers
+                .SelectMany(i => Enumerable.Repeat(i, numberScores[i]))
+                .ToArray();
 
-            int index;
+            string number;
             do
             {
-                int indexIndex = _random.Next(indexes.Length);
-                index = indexes[indexIndex];
-            } while (_turn >= LongestRunAllowed && _lastIndexes.All(i => i == index)); // avoid long runs of identical numbers
+                int index = _random.Next(weightedNumbers.Length);
+                number = weightedNumbers[index];
+            } while (_turn >= LongestRunAllowed && _lastNumbers.All(i => i == number)); // avoid long runs of identical numbers
 
-            _lastOccurrences[index] = _turn;
-            _lastIndexes.Add(index);
-            while (_lastIndexes.Count > LongestRunAllowed)
-                _lastIndexes.RemoveAt(0);
+            _lastOccurrences[number] = _turn;
+            _lastNumbers.Add(number);
+            while (_lastNumbers.Count > LongestRunAllowed)
+                _lastNumbers.RemoveAt(0);
             _turn++;
 
-            return _numbers[index];
+            return number;
         }
     }
 }
